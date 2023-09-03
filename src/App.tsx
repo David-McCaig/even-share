ca29db7d1c4cd5cdbb640d829bbdc1f4ac6ec317
David-McCaig <davidmccaig1@gmail.com>
2023-09-03 13:34:18 +1200

    Set up react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import './App.css'
-import Home from './Pages/Home'
+import "./App.css";
+import Home from "./Pages/Home";
+import { Routes, Route } from 'react-router'
 
 function App() {
-
-
   return (
-    <>
-    <Home/>
-    </>
-  )
+    <Routes>
+      <Route path='/' element={<login/>} />
+      <Route path='/home' element={<Home/>} />
+    </Routes> 
+  );
 }
 
-export default App
+export default App;
