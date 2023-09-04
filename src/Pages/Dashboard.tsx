ef54a1e33b8146445b6bb6d69407bb457de4f589
David-McCaig <davidmccaig1@gmail.com>
2023-09-04 18:27:47 +1200

    Added BalanceSummaryCard component to Dashboard

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,18 +1,21 @@
 import TopBar from "../Components/TopBar";
+import BalanceSummaryCard from "../features/balancesummary/Component/BalanceSummaryCard";
 
 function Dashboard() {
+  const AddExpenseClick = () => {};
 
-  const AddExpenseClick = () => {
+  const AddSettleUpClick = () => {};
 
-  }
-
-  const AddSettleUpClick = () => {
-
-  }
-  
   return (
-    <div className="w-full" >
-      <TopBar currentPage={"Dashboard"} AddExpenseClick={AddExpenseClick} AddSettleUpClick={AddSettleUpClick}/>
+    <div className="w-full">
+      <TopBar
+        currentPage={"Dashboard"}
+        AddExpenseClick={AddExpenseClick}
+        AddSettleUpClick={AddSettleUpClick}
+      />
+      <div className="lg:hidden" >
+        <BalanceSummaryCard />
+      </div>
     </div>
   );
 }
