3716b5741f50f1360e3eb2146b32b1f1c389dac8
David-McCaig <davidmccaig1@gmail.com>
2023-09-20 16:13:07 +1200

    Add form validation to CreateGroupModal component

--- src/features/Navigation/Components/CreateGroupModal.tsx
+++ src/features/Navigation/Components/CreateGroupModal.tsx
@@ -4,6 +4,7 @@ import { selectUser } from "../../authentication/userSlice";
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../../../utils/firebaseconfig";
 import { useFormik } from "formik";
+import * as Yup from "yup";
 import {
   Dialog,
   DialogContent,
@@ -22,46 +23,57 @@ function CreateGroupModal() {
 
   const [groupMemberEmails, setGroupMemberEmails] = useState([""]);
 
+  const openModalClick = () => {
+    formik.values.groupName = "";
+    formik.values.emails = [""];
+    setGroupMemberEmails([""]);
+  }
+
   const addGroupMember = () => {
-    setGroupMemberEmails([...groupMemberEmails, ""]); 
+    setGroupMemberEmails([...groupMemberEmails, ""]);
   };
 
-  const removeGroupMember = (index:number) => {
+  const removeGroupMember = (index: number) => {
     const updatedEmails = [...groupMemberEmails];
-    updatedEmails.splice(index, 1); 
+    updatedEmails.splice(index, 1);
     setGroupMemberEmails(updatedEmails);
   };
+console.log(groupMemberEmails)
+  const validationSchema = Yup.object().shape({
+    emails: Yup.array()
+      .of(Yup.string().email("Invalid email format"))
+      .min(1, "At least one email is required")
+      .required("Emails are required"),
+    groupName: Yup.string().required("Group name is required"),
+  });
 
   const formik = useFormik({
     initialValues: {
       emails: groupMemberEmails,
       groupName: "",
     },
+    validationSchema: validationSchema,
     onSubmit: (values) => {
       const emailArray = values.emails;
       emailArray.push(userInfo.email);
       const groupName = values.groupName;
-      console.log("Form submitted with emails:", {
-        user_group_email: emailArray,
-        user_group_name: values.groupName,
-      });
-      addGroup(emailArray,groupName);
+      addGroup(emailArray, groupName);
     },
+    validateOnChange: false,
+    validateOnBlur: false,
   });
 
-  const addGroup = async(emailArray:string[],groupName:string) => { 
-    
+  const addGroup = async (emailArray: string[], groupName: string) => {
     await addDoc(collection(db, "userGroups"), {
-        user_group_email: emailArray,
-        user_group_name: groupName,
-      });
-      setGroupMemberEmails([""]);
-  }
-
+      user_group_email: emailArray,
+      user_group_name: groupName,
+    });
+    setGroupMemberEmails([""]);
+  };
 
   return (
     <Dialog>
-      <DialogTrigger asChild>
+      <DialogTrigger onClick={openModalClick} asChild>
         <div className="cursor-pointer"> Create Group +</div>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
@@ -75,33 +87,47 @@ function CreateGroupModal() {
           </DialogHeader>
 
           <div className="flex mt-6  items-center">
-              <Label className="text-left mr-3">Group</Label>
-              <Input
+            <Label className="text-left mr-3">Group</Label>
+            <Input
               name={`groupName`}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
               value={formik.values.groupName}
               className="col-span-3"
-              />
-            </div>
+            />
+          </div>
+          {formik.touched.groupName && formik.errors.groupName ? (
+            <div className="text-red-500">{formik.errors.groupName}</div>
+          ) : null}
           <div className="grid gap-4 py-4">
             {groupMemberEmails.map((email, index) => (
-              <div className="flex items-center gap-4" key={index}>
-                <Label className="text-right">Email</Label>
-                <Input
-                  name={`emails[${index}]`}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.emails[index]}
-                  className="col-span-3"
-                />
-                <Button
-                  className="bg-slate-800"
-                  type="button"
-                  onClick={() => removeGroupMember(index)}
-                >
-                  Remove
-                </Button>
+              <div key={index}>
+                <div className="flex items-center gap-4" >
+                  <Label className="text-right">Email</Label>
+                  <Input
+                    name={`emails[${index}]`}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.emails[index]}
+                    className="col-span-3"
+                  />
+
+                  <Button
+                    className="bg-slate-800"
+                    type="button"
+                    onClick={() => removeGroupMember(index)}
+                  >
+                    Remove
+                  </Button>
+                </div>
+                {formik.touched.emails &&
+                formik.touched.emails &&
+                formik.errors.emails &&
+                formik.errors.emails[index] ? (
+                  <div className="text-red-500">
+                    {formik.errors.emails[index]}
+                  </div>
+                ) : null}
               </div>
             ))}
           </div>
