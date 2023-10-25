7fd14499cb8cdada7cfa330b5ddcaecec0928076
David-McCaig <davidmccaig1@gmail.com>
2023-10-25 15:26:48 +1300

    Remove env file

--- src/firebase/firebaseconfig.tsx
+++ src/firebase/firebaseconfig.tsx
@@ -5,24 +5,15 @@ import { getFirestore } from 'firebase/firestore';
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
-const {
-  VITE_REACT_FIREBASE_APP_API_KEY,
-  VITE_REACT_FIREBASE_APP_AUTH_DOMAIN,
-  VITE_REACT_FIREBASE_APP_PROJECT_ID,
-  VITE_REACT_FIREBASE_APP_STORAGE_BUCCKET,
-  VITE_REACT_FIREBASE_APP_MESSAGING_SENDER_ID,
-  VITE_REACT_FIREBASE_APP_MESSAGING_APPLE_ID,
-  VITE_REACT_FIREBASE_APP_MESSAGING_MEASUREMENT_ID,
-} = import.meta.env;
 
 const firebaseConfig = {
-  apiKey: VITE_REACT_FIREBASE_APP_API_KEY,
-  authDomain: VITE_REACT_FIREBASE_APP_AUTH_DOMAIN,
-  projectId: VITE_REACT_FIREBASE_APP_PROJECT_ID,
-  storageBucket: VITE_REACT_FIREBASE_APP_STORAGE_BUCCKET,
-  messagingSenderId: VITE_REACT_FIREBASE_APP_MESSAGING_SENDER_ID,
-  appId: VITE_REACT_FIREBASE_APP_MESSAGING_APPLE_ID,
-  measurementId: VITE_REACT_FIREBASE_APP_MESSAGING_MEASUREMENT_ID,
+  apiKey: 'AIzaSyCzKpSc68iGDFaGNP0VIvhYGab48loEkd4',
+  authDomain: 'split-bill-427e2.firebaseapp.com',
+  projectId: 'split-bill-427e2',
+  storageBucket: 'split-bill-427e2.appspot.com',
+  messagingSenderId: '98401231170',
+  appId: '1:98401231170:web:61f1eed1898e81f42768db',
+  measurementId: 'G-27Q3NY7SXE',
 };
 
 // Initialize Firebase
