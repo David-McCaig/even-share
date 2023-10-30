358b86b6a91b9b6653efed72e1500dbe1de48591
David-McCaig <davidmccaig1@gmail.com>
2023-10-30 16:16:32 +1300

    Add comments to firestoreApi and store

--- src/firestoreApi.tsx
+++ src/firestoreApi.tsx
@@ -1,5 +1,6 @@
 import { createApi, fakeBaseQuery } from '@reduxjs/toolkit/query/react';
 
+//API slice for interacting with Firestore.
 export const firestoreApi = createApi({
   baseQuery: fakeBaseQuery(),
   tagTypes: ['groupExpense','recentActivity','balanceSummary'],
--- src/store.tsx
+++ src/store.tsx
@@ -3,14 +3,17 @@ import userReducer from "../src/features/authentication/userSlice";
 import groupIdSlice from "./features/groupexpense/groupIdSlice";
 import { firestoreApi } from "./firestoreApi";
 
+// Function to setup and configure the Redux store
 export const setupStore = () =>
   configureStore({
     reducer: {
       user: userReducer,
       groupId: groupIdSlice,
+      // Adding RTK Query's API slice reducer for Firestore operations
       [firestoreApi.reducerPath]: firestoreApi.reducer,
     },
     middleware: (getDefaultMiddleware) => {
+      // Get default middleware and concatenate with firestoreApi's middleware for advanced RTK Query functionality
       return getDefaultMiddleware().concat(firestoreApi.middleware);
     },
   });
@@ -20,4 +23,3 @@ export const store = setupStore();
 export type AppStore = ReturnType<typeof setupStore>;
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-
