d84edbf40374003fed0bc3f3e6806eed98e854c5
David-McCaig <davidmccaig1@gmail.com>
2023-11-17 12:18:03 +1300

    Create testimonial component

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import Hero from "../features/landingpage/Components/hero"
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Landing from '../features/landingpage/index';
 
 function LandingPage() {
-  return (
-    <div>
-        <Hero/>
-    </div>
-  )
-}
-
-export default LandingPage
+    useEffect(() => {
+      // Initialize AOS with the desired configuration
+      AOS.init({
+        once: true,
+        disable: 'phone',
+        duration: 700,
+        easing: 'ease-out-cubic',
+      });
+    }, []); // Ensure the effect runs only once on mount
+  
+    return (
+      <div>
+        <Landing />
+      </div>
+    );
+  }
+  
+  export default LandingPage;
--- src/features/landingpage/index.tsx
+++ src/features/landingpage/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from './Components/hero'
+import Testimonials from './Components/Testimonials'
 
 function index() {
   return (
-    <div>index</div>
+    <div>
+      <Hero/>
+    <Testimonials/>
+    </div>
   )
 }
 
