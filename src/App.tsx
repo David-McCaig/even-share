9e518277c1e2d7bbc77a0eef8b77258daf67900b
David-McCaig <davidmccaig1@gmail.com>
2023-12-04 20:16:40 +1300

    Move routes array back to app.tsx. Set up lazy loading for components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,60 @@
-import "./App.css";
-import { useState } from "react";
-import { useLocation } from "react-router";
-import { Routes, Route } from "react-router";
+import React, { lazy, Suspense, useState } from "react";
+import { Routes, Route, Navigate, useLocation } from "react-router";
 import { useAppSelector } from "./hooks/reduxTypeScriptHooks.tsx";
 import { selectUser } from "./features/authentication/userSlice.tsx";
+import Loader from "./Components/LoadingDots.tsx";
 import TopNavBar from "./features/Navigation/Components/TopNavBar.tsx";
 import SideNavBar from "./features/Navigation/Components/SideNavBar.tsx";
 import BalanceSummary from "./features/balancesummary/index.tsx";
 import BalanceSummaryColumn from "./features/balancesummary/Component/BalanceSummaryColumn.tsx";
-import routes from "./routes.tsx";
+
+// Lazy load components
+const LandingPage = lazy(() => import("./Pages/LandingPage"));
+const Login = lazy(() => import("./Pages/LoginPage"));
+const SignUp = lazy(() => import("./Pages/SignUpPage"));
+const Dashboard = lazy(() => import("./Pages/DashboardPage"));
+const GroupExpense = lazy(() => import("./Pages/GroupExpensePage"));
+const RecentActivityPage = lazy(() => import("./Pages/RecentActivityPage"));
+const ProfilePage = lazy(() => import("./Pages/ProfilePage"));
 
 function App() {
   const [showNavBar, setShowNavBar] = useState<boolean>(true);
-
   const userInfo = useAppSelector(selectUser);
-
-  const routesObject = routes(userInfo);
-
   const location = useLocation();
+
+  const routes = [
+    { id: 1, path: "/", element: <LandingPage /> },
+    {
+      id: 2,
+      path: "/login",
+      element: userInfo ? <Navigate to="/dashboard" /> : <Login />,
+    },
+    {
+      id: 3,
+      path: "/signup",
+      element: userInfo ? <Navigate to="/dashboard" /> : <SignUp />,
+    },
+    {
+      id: 4,
+      path: "/dashboard",
+      element: userInfo ? <Dashboard /> : <Navigate to="/login" />,
+    },
+    {
+      id: 5,
+      path: "/group/:id",
+      element: userInfo ? <GroupExpense /> : <Navigate to="/login" />,
+    },
+    {
+      id: 6,
+      path: "/recentactivity",
+      element: userInfo ? <RecentActivityPage /> : <Navigate to="/login" />,
+    },
+    {
+      id: 7,
+      path: "/profile",
+      element: userInfo ? <ProfilePage /> : <Navigate to="/login" />,
+    },
+  ];
 
   return (
     <div className="w-full h-full text-primary-font-color">
@@ -35,11 +72,19 @@ function App() {
         {userInfo && (
           <SideNavBar showNavBar={showNavBar} setShowNavBar={setShowNavBar} />
         )}
-        <Routes>
-          {routesObject.map((route) => (
-            <Route key={route?.id} path={route?.path} element={route?.element} />
-          ))}
-        </Routes>
+        <Suspense
+          fallback={
+            <div className="w-full h-full flex justify-center self-center">
+              <Loader />
+            </div>
+          }
+        >
+          <Routes>
+            {routes.map((route) => (
+              <Route key={route.id} path={route.path} element={route.element} />
+            ))}
+          </Routes>
+        </Suspense>
         {userInfo && (
           <BalanceSummaryColumn>
             <BalanceSummary />
